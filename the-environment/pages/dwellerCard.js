7194a3f564ee9fa0431a0cef8cbebc47ffe8f8d1
Rodolfo Rangel Rodrigues <rrrodrigues1@stefanini.com>
2021-12-10 10:35:23 -0300

    ajustes para prod 2

--- the-environment/pages/dwellerCard.js
+++ the-environment/pages/dwellerCard.js
@@ -13,7 +13,7 @@ export default function DwellerCard(props){
 
     async function setOpen(){
         if(description == "" && !isOpen){
-            const res = await fetch(`${process.env.HOST}api/dwellersApi?name=The+${props.name}`)
+            const res = await fetch(`api/dwellersApi?name=The+${props.name}`)
             const data = await res.json()
             setDescription(data.description);
         }
